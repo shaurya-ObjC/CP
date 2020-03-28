4f231a7addcb6c221ab2329115390d6681021753
Abhishek Neb <abhishek.neb@ymedialabs.com>
2020-03-28 14:26:26 +0530

    Add: iOS Tips

--- CP/LeetCode/Easy/1108. Defanging an IP Address.playground/Contents.swift
+++ CP/LeetCode/Easy/1108. Defanging an IP Address.playground/Contents.swift
@@ -1,2 +1,87 @@
 import UIKit
 
+class Solution {
+    
+    func criticalRouters(numRouters:Int, numLinks:Int, links:[[Int]]) -> [Int]
+    {
+        
+        var graph = [[Int]]()
+        var results = [Int]()
+        func helper(e: [[Int]], v:Int, total:Int) -> Bool {
+            var set = Set<Int>()
+            
+            var firstAdd = false
+            
+            
+            for i in 0..<e.count {
+                
+                //ignores edges that have v as starting or ending point
+                if(e[i][0] == v || e[i][1] == v){
+                    continue
+                }
+                
+                //adding any edge as the starting point, both vertices
+                //will fail if the edges are self edges
+                if(!firstAdd){
+                    set.insert(e[i][0]);
+                    set.insert(e[i][1]);
+                    firstAdd = true;
+                }
+                
+                //if the next edge has one of the edges already in the set, I can visit it
+                if(set.contains(e[i][0]) || set.contains(e[i][1])){
+                    set.insert(e[i][0]);
+                    set.insert(e[i][1]);
+                }
+            }
+            
+            //if total visited elements equals all vertices - removed vertice
+            return set.count == total - 1;
+        }
+        
+        for k in 0..<numRouters {
+            if(!helper(zzz, k, v)){
+                results.append(k)
+            }
+        }
+        
+        
+    }
+}
+
+Solution().criticalRouters(numRouters: 6, numLinks: 5, links: [[1,2], [2,3], [3,4], [4,5], [6,3]])
+
+
+
+//
+//func numberAmazonGoStores(rows:Int,column:Int, grid:[[Int]])-> Int
+//{
+//    // WRITE YOUR CODE HERE
+//    func checkPoint(_ r: Int, _ c: Int, _ g: [[Int]], _ ch: inout [[Bool]]) -> Int {
+//        if r < 0 || c < 0 || r > (g.count - 1) || c > (g[0].count - 1) { return 0 }
+//        if g[r][c] == 0 { return 0 }
+//        if ch[r][c] { return 0 }
+//
+//        ch[r][c] = true
+//        let _ = checkPoint(r, c - 1, g, &ch)
+//        let _ = checkPoint(r - 1, c, g, &ch)
+//        let _ = checkPoint(r, c + 1, g, &ch)
+//        let _ = checkPoint(r + 1, c, g, &ch)
+//
+//        return 1
+//    }
+//
+//    // WRITE YOUR CODE HERE
+//    let originalGrid = grid
+//    guard !originalGrid.isEmpty else { return 0 }
+//    var checked = [[Bool]](repeating: [Bool](repeating: false, count: column), count: rows)
+//    var result = 0
+//    for row in 0..<rows {
+//        for col in 0..<column {
+//            result += checkPoint(row, col, originalGrid, &checked)
+//        }
+//    }
+//
+//    return result
+//
+//}
--- CP/LeetCode/Easy/344. Reverse String.playground/Contents.swift
+++ CP/LeetCode/Easy/344. Reverse String.playground/Contents.swift
@@ -1,45 +1,61 @@
 import UIKit
 
 class Solution {
-    func reverseString(_ s: inout [String]) {
-        
-        // Another Way
-//        let count = s.count - 1
-//
-//        for index in 0..<s.count/2 {
-//            let last = s[count-index]
-//            s[count-index] = s[index]
-//            s[index] = last
-//
-//            if ((index*2 >= count) || ((index*2 + 1) == count)) {
-//                break
-//            }
-//        }
-        
-        
-        
-        // One Way
-        var left = 0
-        var right = s.count - 1
-        
-        while left < right  {
-//            let rVal = s[right]
-//            s[right] = s[left]
-//            s[left] = rVal
+    
+    func criticalRouters(numRouters:Int, numLinks:Int, links:[[Int]]) -> [Int]
+    {
             
-            (s[left], s[right]) = (s[right], s[left])
+            var graph = [[Int]]()
+            var results = [Int]()
+            var discover = [Int]()
+            var low = [Int]()
+            var times = Int()
+            
+            func router(_ child: Int, _ parent: Int) {
+                times += 1
+                discover[child] = times
+                low[child] = times
+                
+                for node in graph[child] {
+                    if (node == parent) {
+                        continue
+                    }
+                    if (discover[node] == 0) {
+                        router(node, child)
+                        low[child] = min(low[child], low[node])
+                        
+                        if (low[node] > discover[child]) {
+                            results.append(child)
+                        }
+                    } else {
+                        low[child] = min(low[child], discover[node])
+                    }
+                }
+            }
+            
+            graph = [[Int]](repeating: [Int](), count: numRouters)
+            discover = [Int](repeating: 0, count: numRouters)
+            low = [Int](repeating: 0, count: numRouters)
+            
+            //create graph
+            for connection in links{
+            let p = connection[0]-1
+                    let q = connection[1]-1
 
+                graph[p].append(q)
+                graph[q].append(p)
+            }
             
-            right -= 1
-            left += 1
+            //loop n times
+            for i in 0..<numRouters {
+                if (discover[i] == 0) {
+                    router(i, -1)
+                }
+            }
             
             
+            return results
         }
-        
-        
-    }
 }
-
-var chars = ["H", "A", "B", "S", "O"]
-Solution().reverseString(&chars)
+Solution().criticalRouters(numRouters: 6, numLinks: 5, links: [[1,2], [2,3], [3,4], [4,5], [6,3]])
 
